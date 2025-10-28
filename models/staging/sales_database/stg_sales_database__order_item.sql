15e53b3bf1b4a1fc18d24203eec671ccc1b257ae
Morgvn Mdln <morgvn.mdln@gmail.com>
2025-10-28 18:08:54 +0100

    fourth commit

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,11 +1,11 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id,
- order_id,
- product_id,
- 1 AS column_1,
- seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
- price as unit_price,
- shipping_cost,
- quantity as item_quantity,
- (price * quantity) + shipping_cost as total_order_item_amount
+select CONCAT(order_id, '_', product_id) AS order_item_id_y,
+ order_id AS order_id_y,
+ product_id AS product_id_y,
+ 1 AS column_1_y,
+ seller_id AS seller_id_y,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
+ price AS unit_price_y,
+ shipping_cost AS shipping_cost_y,
+ quantity AS item_quantity_y,
+ (price * quantity) + shipping_cost AS total_order_item_amount_y
 from {{ source('sales_database', 'order_item') }}
